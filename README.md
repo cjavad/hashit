b101535d66f384305b41f9482525331c8afce5ac
Javad Shafique <javadshafique@hotmail.com>
2018-01-05 11:37:33 +0100

    Fixed longdesc

---
+++ README.md
@@ -0,0 +1,7 @@
+Hashit is an hashing program which can be uses to hash and verify muliple files on a system.
+I got the idea from an ubuntu iso image which have this hash table, so i got the idea to make
+such a program using python.
+
+In version 1.2.2 i fixed some issues with print and if/else
+
+Works with python2 and python3.
--- setup.py
+++ setup.py
@@ -1,15 +1,7 @@
 from setuptools import setup
 from hashit.version import __version__
 
-LONG_DES = """
-Hashit is an hashing program which can be uses to hash and verify muliple files on a system.
-I got the idea from an ubuntu iso image which have this hash table, so i got the idea to make
-such a program using python.
-
-In version 1.2.2 i fixed some issues with print and if/else
-
-Works with python2 and python3.
-"""
+LONG_DES = open("README.md", "r").read()
 
 setup(
     name = "hashit",
