30d14df23d2bd12e75997851b6092f7fa577732c
Javad Shafique <javadshafique@hotmail.com>
2018-02-10 22:27:00 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -17,11 +17,7 @@ setup(
     },
     url="https://github.com/JavadSM/hashit",
     packages=["hashit"],
-    description = """Hashing Application with muliple modes, settings and more!\n
-    Hashit, is an hashing application used as an verification tool, intendet to replace the "standard" linux hashing utilities such as
-    md5sum, sha1sum and so on. One of the main reasons why this program was develop was to create an easy-to-use command line tool for 
-    newcomers and professionals alike to hash/verify files and other data. For more see our homepage at https://javadsm.github.io/hashit
-    """,
+    description = "Hashing Application with muliple modes, settings and more! Hashit, is an hashing application used as an verification tool, intendet to replace the 'standard' linux hashing utilities such as md5sum, sha1sum and so on. One of the main reasons why this program was develop was to create an easy-to-use command line tool for newcomers and professionals alike to hash/verify files and other data. For more see our homepage at https://javadsm.github.io/hashit",
     long_description = open("README.rst", "r").read(),
     classifiers = [
         "Programming Language :: Python :: 2",
